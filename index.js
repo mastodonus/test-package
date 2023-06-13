d73a7b9af590ea930db4ab0ff16d866b683a05f3
Adam Osmond <adam.osmond@garvin-allen.com>
2023-06-13 10:37:36 -0300

    components

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello World');
+import components from'./components'
+
+const plugin = {
+  install (Vue) {
+    for (const prop in components) {
+      if (components.hasOwnProperty(prop)) {
+        const component = components[prop]
+        Vue.component(component.name, component)
+      }
+    }
+  }
+}
+
+export default plugin
